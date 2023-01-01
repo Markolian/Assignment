3cc39a0598f952b13a9bc4f4f2c2a1c5912d2c3f
Marko <marko.pesic16@gmail.com>
2023-01-01 02:43:05 +0100

    Initial Commit

---
+++ src/assignment/glavna.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class glavna {
+public static void main(String[] args) {
+        
+        for (int i = 0; i < 36; i++) {
+    System.out.println("Hello World");
+}
+      
+    }
+    
+}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author assassin
+ *
+ */
+module assignment {
+}
